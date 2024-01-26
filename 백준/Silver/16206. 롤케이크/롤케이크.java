e71c478250d170332c586f70d869f726f7bd2342
Junyeong Bae <78362930+Junyeong-UOS@users.noreply.github.com>
2024-01-26 14:39:09 +0900

    Update 롤케이크.java

--- 백준/Silver/16206. 롤케이크/롤케이크.java
+++ 백준/Silver/16206. 롤케이크/롤케이크.java
@@ -8,33 +8,34 @@ public class Main {
 		
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		
+
+		// 맛있는 롤케이크들 배열
 		int[] cakes = new int[N];
 		for(int i = 0; i<N; i++) {
 			cakes[i] = sc.nextInt();
 		}
 		
-		int cut = 0;
-		Arrays.sort(cakes);
+		int cut = 0; // 자르는 횟수
+		Arrays.sort(cakes); // 오름차순 정렬 => 10의 배수인 경우 작은 크기의 케이크를 먼저 자른다
 
-		int cakePiece = 0;
+		int cakePiece = 0; // 크기가 10인 조각 수
 
 		for(int cake : cakes) {
 			int tmp = cake%10;
 			
 			
 			if(cake == 10) {
-				cakePiece += 1;
+				cakePiece += 1; // 처음 크기가 10이면 자르지 않는다
 			}
 			else if(tmp == 0) {
-				int i = 0;
+				int i = 0; // 조각 당 자르는 횟수
 				while(cut<M && i < cake/10 - 1) {					
 					cut++;
 					i++;
 					cakePiece++;
 				}	
 				
-				if(i == ( (cake/10) - 1) ) {
+				if(i == ( (cake/10) - 1) ) { // 10의 배수인 케이크를 최대로 자른 경우
 					cakePiece++;
 				} 	
 			}
@@ -44,7 +45,7 @@ public class Main {
 		for(int cake : cakes) {
 			int tmp = cake%10;
 			if(tmp != 0) {
-				int i = 0;
+				int i = 0; // 조각 당 자르는 횟수
 				while(cut<M && i < Math.ceil(cake/10.0) - 1) {					
 					cut++;
 					i++;
@@ -54,7 +55,7 @@ public class Main {
 		}
 	
 		
-		System.out.println(cakePiece);
+		System.out.println(cakePiece); // 맛있게 먹는다
 
 	}
 
