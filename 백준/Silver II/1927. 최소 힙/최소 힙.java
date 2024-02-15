6fe285121082e5a85572eb520c5355fec3ddbf29
Junyeong Bae <78362930+Junyeong-UOS@users.noreply.github.com>
2024-02-15 12:26:26 +0900

    [Silver II] Title: 최소 힙, Time: 1044 ms, Memory: 112052 KB -BaekjoonHub

---
+++ 백준/Silver II/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		StringBuilder sb = new StringBuilder();
+		int N = sc.nextInt();
+		for (int n = 0; n < N; n++) {
+			int check = sc.nextInt();
+			switch (check) {
+			case 0:
+				if (pq.isEmpty()) {
+					sb.append("0\n");
+				} else {
+					sb.append(pq.poll() + "\n");
+				}
+				break;
+			default:
+				pq.add(check);
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+}
