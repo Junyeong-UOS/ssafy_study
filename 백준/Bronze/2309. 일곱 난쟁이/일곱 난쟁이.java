83053ec6998a9c18c972b268d3318b5703899ce9
Junyeong Bae <78362930+Junyeong-UOS@users.noreply.github.com>
2024-02-26 17:27:52 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,50 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-		// 난쟁이들
-		int[] dwarfs = new int[9];
-		for (int i = 0; i < 9; i++) {
-			dwarfs[i] = Integer.parseInt(bf.readLine());
-		}
-
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[9];
 		int sum = 0;
-
-		for (int i = 0; i < 9; i++) {
-			sum += dwarfs[i];
+		
+		for(int i = 0; i < 9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
 		}
-
-		int check = sum;
-		int notDwarfs1 = 0;
-		int notDwarfs2 = 0;
-		Arrays.sort(dwarfs);
-		for (int i = 0; i < 8; i++) {
-			for (int j = i + 1; j < 9; j++) {
-				sum -= dwarfs[i];
-				sum -= dwarfs[j];
-				
-				if (sum == 100) {
-					notDwarfs1 = i;
-					notDwarfs2 = j;
+		
+		boolean find = false;
+		for(int i = 0; i < 8; i++) {
+			for(int j = i + 1; j < 9; j++) {
+				if(arr[i] + arr[j] == sum - 100) {
+					arr[i] = 0;
+					arr[j] = 0;
+					find = true;
 					break;
-				} else {
-					sum = check;
 				}
 			}
-		}
-		
-		for(int i=0; i<9; i++) {		
-			if(i!=notDwarfs1 && i!=notDwarfs2) {
-				System.out.println(dwarfs[i]);
+			if(find) {
+				break;
 			}
 		}
+		Arrays.sort(arr);
 		
+		for(int i = 2; i < 9; i++) {
+			System.out.println(arr[i]);
+		}
 	}
-
 }
